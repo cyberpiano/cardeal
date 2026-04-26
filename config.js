58b3430459484dace6793618c678b23924e4557c
unknown <emmanuelfiati32@gmail.com>
2026-04-26 01:57:07 +0000

    fix: Include config.js for GitHub Pages

---
+++ config.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA_7UEiQyAoeirIHMNJyBWItEMSrP0aph4",
+  authDomain: "cardeals-97d4d.firebaseapp.com",
+  projectId: "cardeals-97d4d",
+  storageBucket: "cardeals-97d4d.firebasestorage.app",
+  messagingSenderId: "366209464372",
+  appId: "1:366209464372:web:a22349cf521bd583ef7261",
+  measurementId: "G-764VVTY4F7"
+};
+
+export const IMGBB_KEY = '3b12085d09bfcd292248a03afe861e7f';
